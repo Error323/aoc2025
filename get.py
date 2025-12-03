64fc0029d4d485b8a1978e26774a964e937bb875
Error323 <folkerthuizinga@gmail.com>
2025-12-03 19:04:46 +0100

    typo

--- get.py
+++ get.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 import sys
+import os
 
 from pathlib import Path
 
@@ -8,17 +9,8 @@ now = datetime.datetime.now()
 
 AOC_YEAR = now.year
 DAY = now.day
-SESSION_COOKIE = "53616c7465645f5f03eeeacba6c44355d905edbcae34a27bd05d5873c3c38f4616f96f7e320c21523a7819a1cec5fce876726c8bd535d0bdc63e53e0853d8bc6"
 
 def download_aoc_input(year, day, session_cookie, save_path):
-    """
-    Download the Advent of Code input file for a given day using the session cookie.
-    
-    :param year: The year of the Advent of Code event.
-    :param day: The day of the challenge.
-    :param session_cookie: Your session cookie for adventofcode.com.
-    :param save_path: Path to save the downloaded input file.
-    """
     url = f"https://adventofcode.com/{year}/day/{day}/input"
     headers = {
         "Cookie": f"session={session_cookie}",
@@ -38,7 +30,7 @@ if __name__ == "__main__":
     path = Path(__file__).parent
     save_file_path = path / f"day{DAY}/input.txt"
     save_file_path.parent.mkdir(exist_ok=True)
-    download_aoc_input(AOC_YEAR, DAY, SESSION_COOKIE, save_file_path)
+    download_aoc_input(AOC_YEAR, DAY, os.getenv("AOC_COOKIE"), save_file_path)
     with open(save_file_path, "rt") as f:
         for i, line in enumerate(f):
             if i > 20:
